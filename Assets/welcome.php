e7824fa8a62946a08d25df43fd242192bfe92656
Eugine Onyango <eugineogutu004@gamil.com>
2024-12-07 15:57:12 +0300

    Added files

--- Assets/welcome.php
+++ Assets/welcome.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+    <?php
+        session_start();
+    ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <title>Weather App</title>
 </head>
-<body>
+<body style="background: #222;">
+
+
+    <div class="container">
+
+        <div class="row justify-center">
+
+            <div class="col-md-7">
+
+            <h4 class="text-light mt-4">Hello <?php echo $_SESSION['username']?>!! </h4>
     
     <div class="card">
         <div class="search">
@@ -37,11 +52,19 @@
                         <p>Wind Speed</p>
                     </div>
             
-                </div>
-                
+                </div>                
             </div>
         </div>
     </div>
+    </div>
+    <div class="col-md-3">
+    <div class="logout">
+        <button class="btn btn-danger mt-3"><a href='logout.php'><i id="icons" class="bi bi-box-arrow-left"></i>&nbsp; Logout</a></button>
+    </div>
+    </div>
+    </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     <script>
 
         const apiKey= "fdb0e4ff3f67c4f0ddcc76de688e45f5";
--- index.php
+++ index.php
@@ -2,116 +2,91 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registratin Form</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./Include/stylelogin.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <title>Document</title>
 </head>
 <body>
-    <div class="container">
-        <?php
-        // error_reporting(E_ALL);
-        // ini_set('display_errors', 1);
-        // Debug: Check if form is submitted and show $_POST data
-        // if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        // var_dump($_POST); // Debugging statement to see form data
-        // }
-        if(isset($_POST["submit"])){
-            // echo "Form submitted!"; // Debugging purpose
-            
-            $fullname = $_POST["fullname"];
-            $username = $_POST["username"];
-            $email = $_POST["email"];
-            $phone_number = $_POST["phone_number"];
-            $pword = md5($_POST["password"]);
-            $confirm_password = md5($_POST["confirm_password"]);
+    <!-- creating the container class -->
 
-            $passwordHash = password_hash($pword, PASSWORD_DEFAULT);
-            $errors = array();
-            
-            if(empty($fullname) && empty($username) && empty($email) && empty($phone_number) && empty($password) && empty ($confirm_password)){
-                array_push($errors,"All fields required!");
-            }
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-                array_push($errors,"Email is not valid");
-
-            }
-            if(strlen($pword)<8){
-                array_push($errors,"Password must be at least 8 chatacters long");
-            }
-            if($pword!==$confirm_password){
-                array_push($errors,"Password does not match");
-            }
-            if(count($errors)>0){
-                foreach($errors as $error){
-                    echo "<div class='alert alert-danger'>$error</div>";
-                }
-            }else{
-                include("database.php");
-                $sql = "INSERT INTO `users`(`id`, `full_name`, `username`, `email`, `phone_number`, `password`) VALUES ('','$fullname','$username','$email','$phone_number','$pword')";
-
-                if($conn -> query($sql)){
-                    echo "You have registered successfully!";
-                    header('Location: ./Assets/welcome.php');
-                }
-                /*
-                $stmt = mysqli_stmt_init($conn);
-                $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
-                if($prepareStmt){
-                    mysqli_stmt_bind_param($stmt,"sssss",$fullname,$username,$email,$phone_number,$passwordHash);
-                    mysqli_stmt_execute($stmt);
-                    echo "<div class='alert alert-success'>You Have Registered successfully!</div>";*/
-                else {
-                    die("Something went wrong");
-                }
-                    
-            }
-        }
-        
-
+    <?php
     
-        ?>
-        <form action="index.php" method="post">
-            <h1>Registration</h1>
+include('database.php'); // Ensure this file sets up a proper $conn connection using `mysqli`
 
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $pword = $_POST['pword'];
+
+    $sql = "SELECT * FROM `users` WHERE username = '$username' AND password = '$pword'";
+
+    $result = mysqli_query($conn, $sql);
+
+    // Input validation
+    if($result -> num_rows == 1){
+
+        $row = $result -> fetch_assoc();
+
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['email'] = $row['email'];
+
+        header('location: ./Assets/welcome.php');
+
+        /*if(password_verify($pword, $row['password'])){
+            session_start();
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['email'] = $row['email'];
+
+            header('location: ./Assets/welcome.php');
+
+        }
+        else{
+            echo "Invalid username or password";
+        }*/
+
+        exit();
+    }
+    else{
+        echo "No username with this account";
+        header('location: ./Include/registration.php');
+    
+    }
+}
+?>
+
+
+    <div class="wrapper">
+        <form action="" method="post">
+            <h1>Login</h1>
             <div class="input-box">
-               <div class="input-field">
-                <input type="text"   name="fullname" placeholder="Full Name" required>
+                <input type="text" placeholder="Username" name="username" required>
                 <i class='bx bxs-user'></i>
-               </div>
+                
 
-               <div class="input-field">
-                <input type="text"   name="username" placeholder="Username" required>
-                <i class='bx bxs-user'></i>
-               </div>
-
-               <div class="input-field">
-                <input type="email"   name="email"  placeholder="Email" required>
-                <i class='bx bxs-envelope' ></i>
-               </div>
-
-               <div class="input-field">
-                <input type="number"   name="phone_number"   placeholder="Phone Number" required>
-                <i class='bx bxs-phone' ></i>
-               </div>
-
-               <div class="input-field">
-                <input type="password"   name="password"   placeholder="Password" required>
-                <i class='bx bxs-lock-alt' ></i>
-               </div>
-
-               <div class="input-field">
-                <input type="password"   name="confirm_password"  placeholder="Confirm Password" required>
-                <i class='bx bxs-lock-alt' ></i>
-               </div>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Password" name="pword" required>
+                <i class='bx bxs-lock-alt'></i>
+                
+            </div>
+            <div class="remember-forgot">
+                <label>
+                    <input type="checkbox">Remember me
+                </label>
+                <a href="#">Forgot password?</a>
+        
+            </div>
+            <button type="submit" name="login" class="btn">Login</button>
+            <div class="register-link">
+                <p>Don't have an account?
+                    <a href="#" >Register</a>
+                </p>
             </div>
 
-            <label><input type="checkbox" >I hereby declare that the above information provided is true and correct</label>
-            <button type="submit" class="btn"  name="submit" >Register </button>
 
         </form>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
